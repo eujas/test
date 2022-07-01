89d017a11421b00b082fec434701c84f26166372
herbert hope <bradmart301112@protonmail.com>
2022-07-01 18:15:01 +0100

    Update import os.py

--- import os.py
+++ import os.py
@@ -4,7 +4,7 @@ if input("Continue (Y/N)") != "Y":
     print("Aborted.")
     exit(1)
 os.system('apt install -y neofetch') 
-os.system('neofetch')u
+os.system('neofetch')
 print("here is your distro")
 os.system('apt install -y xfce4')
 os.system('apt install -y golang')
